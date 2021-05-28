388069dcabdf5e86de68692d322735df3dc3a193
dev_Not_Integer <dev_qkrwjdtn1236@users.noreply.github.com>
2021-05-29 05:44:20 +0900

    fix ememy animator

--- Assets/Scripts/Enermy.cs
+++ Assets/Scripts/Enermy.cs
@@ -6,11 +6,13 @@ public class Enermy : MonoBehaviour
 {
     public Transform target;
     private Rigidbody rig;
+    private Animator ani;
     public float speed = 20f;
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody>();
+        ani = GetComponent<Animator>();
         target = GameObject.FindWithTag("Player").transform;
     }
 
@@ -23,9 +25,13 @@ public class Enermy : MonoBehaviour
             // Debug.Log(Time.deltaTime);
             rig.AddForce(trans*speed*Time.deltaTime);
             // 플레이어 보기
-
+            ani.SetBool("walk",true);
             //transform.LookAt(target);
             transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(trans),5*Time.deltaTime);
+        }
+        else
+        {
+            ani.SetBool("walk", false);
         }
     }
 }
